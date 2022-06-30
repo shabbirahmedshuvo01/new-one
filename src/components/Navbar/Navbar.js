279d84e2a3b3032f8b4905664485084328529add
Sabbir Shuvo <Shabbir Ahmed Shuvo>
2022-07-01 04:53:36 +0600

    navbar done

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Navbar.css'
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <div className='bg-teal-300'>
+            <nav className='header'>
+                <Link to={'/'}><h2 className='text-2xl font-bold'>Tasks work!!!</h2></Link>
+                <div>
+                    <Link to={'/'}>Home</Link>
+                    <Link to={'/projects'}>Projects</Link>
+                    <Link to={'/blogs'}>Blogs</Link>
+                    <Link to={'/login'}>Login</Link>
+                    <Link to={'/about'}>About</Link>
+                </div>
+            </nav >
+        </div>
+    );
+};
+
+export default Navbar;
